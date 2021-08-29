c25f8139113646f0afeee52c1019a1dfe3d19ddb
GeoSomu <somu.career2420@gmail.com>
2021-08-29 11:59:24 +0530

    nav bar created

--- ng-redbus-model/src/app/app.component.ts
+++ ng-redbus-model/src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-redbus-model';
+  visible: boolean = false;
+
+  Sources = ['Adambakkam','Adayar', 'Alandur','Ambatthur',  'Anna Nagar', 'Ashok Nagar', 'Avadi', 'BroadWay', 'Chromepet', 'Egmore', 'Guindy', 'Injampakkam',
+    'K.K. Nagar', 'Kodampakkam', 'Korukupet', 'Mathavaram', 'Mambalam', 'Medavakkam', 'Mylapore', 'Nungampakkam', 'Pallavaram', 'Perambur',
+    'Perungudi', 'Royapettah', 'Saidapet', 'Sholinganallur','Tambaram', 'T.Nagar', 'Teynampet', 'Triplicane', 'Vadapalani', 'Vandalur','Velachery'
+    , 'Vyasarpadi', 'Washermanpet'
+  ];
+
+  Destinations = ['Adambakkam','Adayar', 'Alandur','Ambatthur',  'Anna Nagar', 'Ashok Nagar', 'Avadi', 'BroadWay', 'Chromepet', 'Egmore', 'Guindy', 'Injampakkam',
+    'K.K. Nagar', 'Kodampakkam', 'Korukupet', 'Mathavaram', 'Mambalam', 'Medavakkam', 'Mylapore', 'Nungampakkam', 'Pallavaram', 'Perambur',
+    'Perungudi', 'Royapettah', 'Saidapet', 'Sholinganallur','Tambaram', 'T.Nagar', 'Teynampet', 'Triplicane', 'Vadapalani', 'Vandalur','Velachery'
+    , 'Vyasarpadi', 'Washermanpet'
+  ]
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    setTimeout(() => {
+      this.visible = true
+    }, 3000);
+  }
 }
